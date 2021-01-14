45a13ffb6395f9d2876768eee00cb8d5b02c57e6
Himanshi <himanshigarg1312@gmail.com>
2021-01-14 15:24:52 +0530

    setting up repositories

---
+++ src/main/java/guru/springframework/spring5webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.spring5webapp.repositories;
+
+import guru.springframework.spring5webapp.domain.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public class BookRepository extends CrudRepository<Book, Long> {
+}
